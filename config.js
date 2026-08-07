1d03ec6390aac9661f1d13bdbe8a81d0d5f8fc77
pet-salon-manager <sinntyoukoukichild@yahoo.co.jp>
2026-08-08 01:22:37 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.UCHINOKO_CONFIG = {
+  SUPABASE_URL: 'https://abofyfovxysqzchstnbx.supabase.co',
+  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_pslo3qPFj50r2AQx_zx0ew_ywzBdxIj',
+  STORAGE_BUCKET: 'pet-files'
+};
